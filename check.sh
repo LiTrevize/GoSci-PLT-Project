9e8cda346c0866c657444b00c4ac54e0e74ed7a0
Jingyu Li <jingyu.li@columbia.edu>
2022-04-27 19:26:41 -0400

    feat: add integration test script

--- check.sh
+++ check.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 echo "...format *.ml" &&
 ocamlformat -i *.ml &&
+echo "...check parser with ocamlyacc"
+ocamlyacc gosciparse.mly
+rm gosciparse.ml gosciparse.mli
 echo "...build main.native" &&
 ocamlbuild -pkgs llvm main.native && 
+echo "...run main.native on example code" &&
 ./main.native scan < example.gs 1> /dev/null &&
 ./main.native parse < example.gs 1> /dev/null &&
 ./main.native scheck < example.gs 1> /dev/null &&
 ./main.native irgen < example-ir.gs 1> /dev/null &&
 
-echo "...build test.native" &&
-ocamlbuild -use-ocamlfind -pkgs ounit2 test.native &&
-echo "...run tests" && ./test.native
+echo ""
+./run_unit_tests.sh
+
+echo ""
+./run_integration_tests.sh
