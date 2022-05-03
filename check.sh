9b26c544a1deae24fc6f34d9f71cb53ad436b7af
Edward Z. Ma <edward.maze.cu@gmail.com>
2022-05-03 19:34:28 -0400

    Update check.sh to fail fast.

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "...format *.ml" &&
 ocamlformat -i *.ml &&
-echo "...check parser with ocamlyacc"
-ocamlyacc gosciparse.mly
-rm gosciparse.ml gosciparse.mli
+echo "...check parser with ocamlyacc" &&
+ocamlyacc gosciparse.mly &&
+rm gosciparse.ml gosciparse.mli &&
 echo "...build main.native" &&
 ocamlbuild -pkgs llvm main.native && 
 echo "...run main.native on example code" &&
@@ -12,8 +12,8 @@ echo "...run main.native on example code" &&
 ./main.native scheck < example.gs 1> /dev/null &&
 ./main.native irgen < example-ir.gs 1> /dev/null &&
 
-echo ""
-./run_unit_tests.sh
+echo "" &&
+./run_unit_tests.sh &&
 
-echo ""
-./run_integration_tests.sh
+echo "" &&
+./run_integration_tests.sh 
