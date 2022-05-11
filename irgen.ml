d0614412195e797b4afb7ac249654310b478f2b6
DYnamoX513 <923948485@qq.com>
2022-05-11 18:25:34 -0400

    fix minor bug in switch

--- irgen.ml
+++ irgen.ml
@@ -597,7 +597,7 @@ let translate ((sglobals, units, utypes, functions) : sprogram) =
               (*default case*)
               ignore (List.map (fun stmt -> build_stmt local_vars switch_target (L.builder_at_end context default_bb) stmt) sl);
               add_terminal (L.builder_at_end context default_bb) (L.build_br end_bb);
-              dests
+              (L.undef i32_t, default_bb) :: dests
             | _ -> 
               (*case expr1, expr2... : 
                  stmt list*)
@@ -611,8 +611,11 @@ let translate ((sglobals, units, utypes, functions) : sprogram) =
               case_pairs @ dests)) 
           [] (List.rev casel) in
         add_terminal (L.builder_at_end context default_bb) (L.build_br end_bb);
-        let sw = L.build_switch case_value default_bb (List.length all_cases) builder in
-        ignore(List.map (fun dest -> L.add_case sw (fst dest) (snd dest)) all_cases);
+        let default_count = List.fold_left (fun count case -> 
+          match case with 
+          | (_, b) -> if b == default_bb then count + 1 else count) 0 all_cases in 
+        let sw = L.build_switch case_value default_bb ((List.length all_cases)-default_count) builder in
+        ignore(List.map (fun dest -> if snd dest != default_bb then L.add_case sw (fst dest) (snd dest)) all_cases);
         L.builder_at_end context end_bb
       | SMatchS (_, s, e, case_list) ->
         let (vt, _), e' = e in
